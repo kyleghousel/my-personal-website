c63f923c876752f5585116f5b1833f7be7bf8779
Kyle <kylegranthousel@gmail.com>
2024-02-26 17:23:19 -0500

    Final commit. Touched up README file grammar, added W3C CSS Validation, and made some final adjustments.

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const header = document.getElementById("main-header");
 for (let i = 0; i < lists.length; i++) {
   lists[i].addEventListener("mouseover", function() {
     this.style.transform = "scale(1.1)";
+    this.style.transition = "transform 0.5s ease"
     this.style.border = "none";
     this.style.padding = "16px";
   });
   lists[i].addEventListener("mouseout", function() {
     this.style.transform = "scale(1)";
+    this.style.transition = "transform 0.7s ease"
     this.style.border = "8px solid white";
     this.style.padding = "8px";
 });
